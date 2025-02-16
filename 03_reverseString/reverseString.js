5098eea80dcded6bb7b1397f32c589d84e4e64b5
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-02-16 15:23:09 -0500

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let characters = string.split('');
 
+    let reversedString = "";
+
+    for (let i = characters.length - 1; i >= 0; i--) {
+        reversedString += characters[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line

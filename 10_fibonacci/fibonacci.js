69e876951daa03f51b7896ad1ce95f44fba0dfa7
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-03-04 16:58:40 +0000

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    let fib = [0, 1]
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i -2]
+    }
+
+    return fib[num]
 };
 
 // Do not edit below this line

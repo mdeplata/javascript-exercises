4bcb152ad0cfea7799543503aad3cd4fa876ec6a
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-03-04 17:12:51 +0000

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        let aAge = 0;
+        let bAge = 0;
 
+        if (!a.yearOfDeath) {
+            aAge = 2025 - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+
+        if (!b.yearOfDeath) {
+            bAge = 2025 - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return aAge > bAge ? -1 : 1;
+    })
+
+    return arr[0];
 };
 
 // Do not edit below this line

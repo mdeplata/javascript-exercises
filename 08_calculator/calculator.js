69fa1e5bb0a4343a169b1a120dd508ffd2e157ec
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-03-03 20:26:05 -0500

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(one, two) {
+  return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let i = 0; i < array.length ; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(exp, power) {
+	return exp ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line

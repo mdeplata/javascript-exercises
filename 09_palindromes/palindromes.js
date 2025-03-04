9e9a076dc90a98dc31cad8f51ff815f7cf9ea57d
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-03-04 16:49:42 +0000

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let remPunc = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    console.log("Removed punctuation string is: " + remPunc);
+    let remSpace = remPunc.replace(/\s+/g, '').toLowerCase();
+    console.log("Remove space string is: " + remSpace);
+    let rev = remSpace.split("").reverse().join("");
+    console.log("Reversed string is: " + rev)
 
+    if (remSpace == rev) {
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
